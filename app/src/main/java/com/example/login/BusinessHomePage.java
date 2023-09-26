e4d383680de4f262467c2fe15368ff261b8e1963
Ruan486 <s224048805@mandela.ac.za>
2023-09-26 22:36:58 +0200

    git commit

--- app/src/main/java/com/example/login/BusinessHomePage.java
+++ app/src/main/java/com/example/login/BusinessHomePage.java
@@ -1,22 +1,216 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BusinessHomePage extends AppCompatActivity {
     private int businessID;
+    private List<Event> events;
+    private List<Business> business;
+    private businessEventDisplayAdapter adapter2;
+    private RecyclerView rc1 ;
+    private RecyclerView rc2;
+    private TextView txtBusinessName;
+    private ImageView imgDisplayPic2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        makeNice();
         setContentView(R.layout.activity_business_home_page);
+        txtBusinessName = findViewById(R.id.txtBusinessName);
+        imgDisplayPic2 = findViewById(R.id.imgBusDisplayPic);
         SharedPreferences sharedPref = getSharedPreferences("MyPrefs2", Context.MODE_PRIVATE);
         businessID = sharedPref.getInt("businessID", -1);
-
+        events = new ArrayList<>();
+        business = new ArrayList<>();
+        adapter2 = new businessEventDisplayAdapter(events);
+        rc2 = findViewById(R.id.rcEvents);
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rc2.setLayoutManager(layoutManager2);
+        rc2.setAdapter(adapter2);
+        rc2.addItemDecoration(new SpaceItemDecoration(15));
+        new getBusinessDataQueryAsyncTask().execute(businessID);
+        new EventQueryTask().execute(businessID);
 
     }
+    private class getBusinessDataQueryAsyncTask extends  AsyncTask<Integer, Void, List<Business>> {
+
+        @Override
+        protected List<Business> doInBackground(Integer... integers) {
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            List<Business> fetchedBusinesses = new ArrayList<>();
+            try {
+                String sql = "Select * from business WHERE BusinessID = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setInt(1,businessID);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                int businessID = resultSet.getInt("BusinessID");
+                String Email = resultSet.getString("Email");
+                String password = resultSet.getString("Password");
+                String BusinessName = resultSet.getString("BusinessName");
+                String ContactNumber = resultSet.getString("ContactNumber");
+                String Specials = resultSet.getString("Specials");
+                int Capacity = resultSet.getInt("CapacityLimit");
+                String BusType = resultSet.getString("BusType");
+                String Location = resultSet.getString("Location");
+                byte[] imageData1 = resultSet.getBytes("Image1");
+
+                if (imageData1 != null && imageData1.length > 0) { // Check if imageData1 has a value
+                    Bitmap bitmap1 = BitmapFactory.decodeByteArray(imageData1, 0, imageData1.length);
+                    Business business2 = new Business(businessID, Email, BusinessName, ContactNumber, password, Specials, Capacity, BusType, Location);
+                    business2.setImage1(bitmap1);
+                    fetchedBusinesses.add(business2);
+                }
+                else
+                {
+                   Business business2 = new Business(businessID, Email, BusinessName, ContactNumber, password, Specials, Capacity, BusType, Location);
+                   fetchedBusinesses.add(business2);
+                   // imgDisplayPic2.setImageResource(R.drawable.img);
+                }
+
+                resultSet.close();
+                preparedStatement.close();
+
+
+
+            } catch (Exception e) {
+                e.getMessage();
+
+
+            }
+
+
+        return  fetchedBusinesses;
+        }
+
+        @Override
+        protected void onPostExecute(List<Business> business)
+        {
+            if (business!=null)
+            {
+
+                txtBusinessName.setText(business.get(0).getName());
+                if (business.get(0).getImage1() != null ) {
+                    imgDisplayPic2.setImageBitmap(business.get(0).getImage1());
+                } else {
+                    imgDisplayPic2.setImageResource(R.drawable.img);
+                }
+            }
+        }
+
+    }
+
+
+
+
+    private void makeNice() {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(Color.parseColor("#FFF1F1F1"));
+        }
+
+        // Make the navigation bar immersive
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+
+        // Change the status bar icons color to black
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+    }
+    private class EventQueryTask extends AsyncTask<Integer, Void, List<Event>> {
+
+
+
+        @Override
+        protected List<Event> doInBackground(Integer... Integer) {
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            List<Event> fetchedEvents = new ArrayList<>();
+
+            try {
+                String sql = "Select EventID from businessevents WHERE BusinessID = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setInt(1,businessID);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                   while (resultSet.next()) {
+                       int EventID = resultSet.getInt("EventID");
+                       String sql2 = "Select * FROM events WHERE EventID = ?";
+                       PreparedStatement preparedStatement1 = connection.prepareStatement(sql2);
+                       preparedStatement1.setInt(1, EventID);
+                       ResultSet resultSet1 = preparedStatement1.executeQuery();
+                       while (resultSet1.next()) {
+                           int EventID2 = resultSet1.getInt("EventID");
+                           String name = resultSet1.getString("EventName");
+                           String eventdate = resultSet1.getString("EventDate");
+                           String eventtime = resultSet1.getString("EventTime");
+                           Double ticket = resultSet1.getDouble("TicketPrice");
+                           String venue = resultSet1.getString("Venue");
+                           int capacity = resultSet1.getInt("CapacityLimit");
+                           int age = resultSet1.getInt("AgeRestriction");
+                           String reoccuring = resultSet1.getString("Recurring");
+                           int rating = resultSet1.getInt("Rating");
+                           String desc = resultSet1.getString("Description");
+                           byte[] imageData1 = resultSet1.getBytes("Image1");
+                           Bitmap bitmap1 = BitmapFactory.decodeByteArray(imageData1, 0, imageData1.length);
+                           fetchedEvents.add(new Event(EventID2, name, eventdate, eventtime, ticket, venue, capacity, age, reoccuring, desc, rating, bitmap1));
+                       }
+                       Log.d("BusinessHomePage", "Number of events retrieved: " + fetchedEvents.size());
+                       resultSet1.close();
+                       preparedStatement1.close();
+                   }
+
+                resultSet.close();
+                preparedStatement.close();
+
+            }catch (Exception e)
+            {
+
+                e.getMessage();
+            }
+            return fetchedEvents;
+        }
+
+        @Override
+        protected void onPostExecute(List<Event> eventData) {
+            if(eventData != null)
+            {
+                events.addAll(eventData);
+                adapter2.notifyDataSetChanged();
+                rc2.setAdapter(adapter2);
+            }
+
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/login/businessEventDisplayAdapter.java
@@ -0,0 +1,77 @@
+package com.example.login;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class businessEventDisplayAdapter extends RecyclerView.Adapter<businessEventDisplayAdapter.BusinessEventViewHolder> {
+
+    private View.OnClickListener onClickListener;
+    public static class BusinessEventViewHolder extends RecyclerView.ViewHolder {
+        public TextView lblEventName;
+        public TextView lbldate;
+        public ImageView EventImage;
+        public Event event;
+
+        public BusinessEventViewHolder(@NonNull View view) {
+            super(view);
+            lblEventName = view.findViewById(R.id.txtEventCard);
+            EventImage = view.findViewById(R.id.imgEventCard);
+            lbldate = view.findViewById(R.id.txtDate);
+
+        }
+
+        public void setEvents(Event event) {
+            this.event = event;
+
+            if (event.getImage1() != null ) {
+                EventImage.setImageBitmap(event.getImage1());
+            } else {
+                EventImage.setImageResource(R.drawable.img);
+            }
+            lblEventName.setText(event.getName());
+            lbldate.setText(event.getDate());
+
+
+        }
+
+
+    }
+    private final List<Event> events;
+    public businessEventDisplayAdapter(List<Event> events ){
+        this.events = events;
+    }
+    @NonNull
+    @Override
+    public BusinessEventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.display_businessevent_card, parent, false);
+        return new BusinessEventViewHolder(view);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull BusinessEventViewHolder holder, int position) {
+        Event eventtemo = events.get(position);
+        holder.setEvents(eventtemo);
+        holder.itemView.setOnClickListener(onClickListener);
+    }
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+}
+
