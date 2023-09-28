e1ee738cbd7f41feb7a6e84e0b13cc294f820e88
Angus pc <anguswaterson@gmail.com>
2023-09-28 14:20:37 +0200

    git commit

--- app/src/main/java/com/example/login/EventAdd.java
+++ app/src/main/java/com/example/login/EventAdd.java
@@ -2,13 +2,227 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Calendar;
 
 public class EventAdd extends AppCompatActivity {
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+    private EditText Name;
+    private EditText Description;
+    private EditText time;
+    private CheckBox AgeRestrict;
+    private EditText Capacity;
+    private ImageView imageView;
+    private Button Weekly;
+    private Button Monthly;
+    private Button AddPhotos;
+    private Button OnceOff;
+    private Button Done;
+    private ImageButton Return;
+    private Button preview;
+    private Button Next;
+    private int  PICK_IMAGE_MULTIPLE = 1;
+    private String imageEncoded;
+    private ArrayList<Uri> mArrayUri;
+    private int position = 0;
+    private List<String> imagesEncodedList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_add);
+        int PICK_IMAGE_MULTIPLE = 1;
+        Name = findViewById(R.id.editTextTextPersonName2);
+        Description = findViewById(R.id.DescriptionText);
+        time = findViewById(R.id.editTextTime);
+        AgeRestrict = findViewById(R.id.checkBox);
+        Capacity = findViewById(R.id.editTextNumber);
+        Weekly = findViewById(R.id.button6);
+        Monthly = findViewById(R.id.button7);
+        OnceOff = findViewById(R.id.button5);
+        AddPhotos = findViewById(R.id.button8);
+        preview = findViewById(R.id.button12);
+        Done = findViewById(R.id.button13);
+        imageView = findViewById(R.id.imageView8);
+        Return = findViewById(R.id.imageButton2);
+        Next = findViewById(R.id.button9);
+        mArrayUri = new ArrayList<Uri>();
+
+
+
+        initDatePicker();
+        dateButton = findViewById(R.id.button4);
+        dateButton.setOnClickListener(event -> {
+            datePickerDialog.show();
+        });
+
+        AddPhotos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // initialising intent
+                Intent intent = new Intent();
+
+                // setting type to select to be image
+                intent.setType("image/*");
+
+                // allowing multiple image to be selected
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_MULTIPLE);
+            }
+        });
+
+
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (position < mArrayUri.size() - 1) {
+                    // increase the position by 1
+                    position++;
+                    imageView.setImageURI(mArrayUri.get(position));
+                } else {
+                    Toast.makeText(EventAdd.this, "Last Image Already Shown", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        preview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (position > 0) {
+                    // decrease the position by 1
+                    position--;
+                    imageView.setImageURI(mArrayUri.get(position));
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // When an Image is picked
+        if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK && null != data) {
+            // Get the Image from data
+            if (data.getClipData() != null) {
+                ClipData mClipData = data.getClipData();
+                int cout = data.getClipData().getItemCount();
+                for (int i = 0; i < cout; i++) {
+                    // adding imageuri in array
+                    Uri imageurl = data.getClipData().getItemAt(i).getUri();
+                    mArrayUri.add(imageurl);
+                }
+                // setting 1st selected image into image switcher
+                imageView.setImageURI(mArrayUri.get(0));
+                position = 0;
+            } else {
+                Uri imageurl = data.getData();
+                mArrayUri.add(imageurl);
+                imageView.setImageURI(mArrayUri.get(0));
+                position = 0;
+            }
+        } else {
+            // show this if no image is selected
+            Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+
+
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
     }
 }
