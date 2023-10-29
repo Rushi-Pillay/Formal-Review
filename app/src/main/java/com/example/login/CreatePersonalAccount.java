76572da40b28cb6e608777f985c8859e82ee476f
Rushen Pillay <rushenudesh@gmail.com>
2023-10-29 21:47:38 +0200

    git commit

--- app/src/main/java/com/example/login/CreatePersonalAccount.java
+++ app/src/main/java/com/example/login/CreatePersonalAccount.java
@@ -1,5 +1,6 @@
 package com.example.login;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.view.View;
@@ -27,6 +28,12 @@ public class CreatePersonalAccount extends AppCompatActivity {
     Statement statement;
     Connection connection;
     boolean found =false;
+    EditText uName;
+    EditText Sur ;
+    EditText Name ;
+    EditText id ;
+    EditText Email;
+    EditText Pass ;
 
 
     @Override
@@ -59,44 +66,68 @@ public class CreatePersonalAccount extends AppCompatActivity {
         getData();
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-
-        try {
-
-
-            String insertQuery = "SELECT * FROM Users WHERE Email = '" + email + "';";
-
-            statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-            ResultSet  rs = statement.executeQuery(insertQuery);
-            if (rs.next()){
-                int aid = rs.getInt("UserID");
-                String title = rs.getString("Email");
-            }
-            else {
-                String insQuery = "INSERT INTO Users (Username, FirstName, LastName, DateOfBirth, Email, Password, DisplayPicture) VALUES" +
-                        "('" + username + "', '" + name + "', '" + surname + "', '" + formattedDOB + "', '" + email + "', '" + password + "','null');";
-
-
-                statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-                statement.executeUpdate(insQuery);
-                Toast.makeText(this,"Account Created!",Toast.LENGTH_LONG).show();
-            }
-            Toast.makeText(this, "Please use a different email, this one is already in use", Toast.LENGTH_SHORT).show();
-
-
+        if (username.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter a username", Toast.LENGTH_SHORT).show();
+            uName.setBackgroundResource(R.drawable.edit_text_border_red); // Set a red border
+        } if (name.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter a name", Toast.LENGTH_SHORT).show();
+            Name.setBackgroundResource(R.drawable.edit_text_border_red); // Set a red border
+        } if (surname.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter a surname", Toast.LENGTH_SHORT).show();
+            Sur.setBackgroundResource(R.drawable.edit_text_border_red); // Set a red border
+        } if (idnum.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter an ID number", Toast.LENGTH_SHORT).show();
+            id.setBackgroundResource(R.drawable.edit_text_border_red); // Set a red border
+        }if (email.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter an email address", Toast.LENGTH_SHORT).show();
+            Email.setBackgroundResource(R.drawable.edit_text_border_red); // Set a red border
+        } if (password.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter a password", Toast.LENGTH_SHORT).show();
+            Pass.setBackgroundResource(R.drawable.edit_text_border_red); // Set a red border
+        } if (username.isEmpty() && name.isEmpty() && surname.isEmpty() && idnum.isEmpty() && email.isEmpty() && password.isEmpty()){
+            // All fields have values, reset borders to default or proceed with further processing
+            uName.setBackgroundResource(R.drawable.default_edit_text_border);
+            Name.setBackgroundResource(R.drawable.default_edit_text_border);
+            Sur.setBackgroundResource(R.drawable.default_edit_text_border);
+            id.setBackgroundResource(R.drawable.default_edit_text_border);
+            Email.setBackgroundResource(R.drawable.default_edit_text_border);
+            Pass.setBackgroundResource(R.drawable.default_edit_text_border);
+            // Continue with your logic or initiate some action
             try {
-                connection.close();
-                connection = null;
+                String insertQuery = "SELECT * FROM Users WHERE Email = '" + email + "';";
+                statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+                ResultSet  rs = statement.executeQuery(insertQuery);
+                if (rs.next()){
+                    int aid = rs.getInt("UserID");
+                    String title = rs.getString("Email");
+                    errorText.setTextColor(Color.parseColor("#FF0000"));
+                    Toast.makeText(this, "Please use a different email, this one is already in use", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    errorText.setTextColor(Color.parseColor("#000000"));
+                    String insQuery = "INSERT INTO Users (Username, FirstName, LastName, DateOfBirth, Email, Password, DisplayPicture) VALUES" +
+                            "('" + username + "', '" + name + "', '" + surname + "', '" + formattedDOB + "', '" + email + "', '" + password + "','null');";
+                    statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                    statement.executeUpdate(insQuery);
+                    Toast.makeText(this,"Account Created!",Toast.LENGTH_LONG).show();
+                }
+
+
+
+                try {
+                    connection.close();
+                    connection = null;
+                } catch (SQLException e) {
+                    e.printStackTrace();
+
+                }
+
             } catch (SQLException e) {
-                e.printStackTrace();
+                copyToClipboard(e.toString());
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();;
 
             }
-
-        } catch (SQLException e) {
-            copyToClipboard(e.toString());
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();;
-
         }
-
 
     }
     private void copyToClipboard(String text) {
@@ -110,31 +141,36 @@ public class CreatePersonalAccount extends AppCompatActivity {
     private void getData() {
 
 
-        EditText uName = findViewById(R.id.usernameEditText);
+         uName = findViewById(R.id.usernameEditText);
+         Sur = findViewById(R.id.surnameEditText);
+         Name = findViewById(R.id.nameEditText);
+         id = findViewById(R.id.IDEditText);
+         Email = findViewById(R.id.edtEmail);
+         Pass = findViewById(R.id.edtPass);
 
         username = uName.getText().toString();
 
-        EditText Name = findViewById(R.id.nameEditText);
+
         name = Name.getText().toString();
 
-        EditText Sur = findViewById(R.id.surnameEditText);
+
         surname = Sur.getText().toString();
 
-        EditText id = findViewById(R.id.IDEditText);
+
         idnum = id.getText().toString();
+        if (idnum==""){
+            String yearPart = idnum.substring(0, 2);
+            String monthPart = idnum.substring(2, 4);
+            String dayPart = idnum.substring(4, 6);
+            formattedDOB = yearPart + "-" + monthPart + "-" + dayPart;
+        }
 
-        String yearPart = idnum.substring(0, 2);
-        String monthPart = idnum.substring(2, 4);
-        String dayPart = idnum.substring(4, 6);
-
-        formattedDOB = yearPart + "-" + monthPart + "-" + dayPart;
 
 
-        EditText Email = findViewById(R.id.edtEmail);
+
         email = Email.getText().toString();
-
-        EditText Pass = findViewById(R.id.edtPass);
         password = Pass.getText().toString();
+
     }
 
 }
